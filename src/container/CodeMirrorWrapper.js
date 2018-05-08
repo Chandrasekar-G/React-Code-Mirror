6f3adf6ad17639f133e8a8b43ae2863e06c6556f
Chandrasekar Gokulanathan <g.chandrasekar1993@gmail.com>
2018-05-08 16:26:22 +0530

    Make the second editor read only

--- src/container/CodeMirrorWrapper.js
+++ src/container/CodeMirrorWrapper.js
@@ -7,11 +7,11 @@ class CodeMirrorWrapper extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            myVal: "a",
+            myVal: "<h1>React</h1>",
             theme: "material",
             mode: "xml",
             allThemes: ["material", "3024-day", "3024-night", "abcdef", "ambiance", "base16-dark", "base16-light", "bespin", "blackboard", "cobalt", "colorforth", "dracula", "duotone-dark", "duotone-light", "eclipse", "elegant", "erlang-dark", "gruvbox-dark", "hopscotch", "icecoder", "idea", "isotope", "lesser-dark", "liquibyte", "lucario", "mbo", "mdn-like", "midnight", "monokai", "neat", "neo", "night", "oceanic-next", "panda-syntax", "paraiso-dark", "paraiso-light", "pastel-on-dark", "railscasts", "rubyblue", "seti", "shadowfox", "solarized", "the-matrix", "tomorrow-night-bright", "tomorrow-night-eighties", "ttcn", "twilight", "vibrant-ink", "xq-dark", "xq-light", "yeti", "zenburn"],
-            allModes: ["apl", "asciiarmor", "asn.1", "asterisk", "brainfuck", "clike", "clike", "cobol", "clike", "clojure", "clojure", "css", "cmake", "coffeescript", "commonlisp", "cypher", "python", "crystal", "css", "sql", "d", "dart", "diff", "django", "dockerfile", "dtd", "dylan", "ebnf", "ecl", "clojure", "eiffel", "elm", "htmlembedded", "htmlembedded", "erlang", "sql", "factor", "fcl", "forth", "fortran", "mllike", "gas", "gherkin", "gfm", "go", "groovy", "haml", "haskell", "haskell-literate", "haxe", "haxe", "htmlembedded", "htmlmixed", "http", "idl", "pug", "clike", "htmlembedded", "javascript", "javascript", "javascript", "jsx", "jinja2", "julia", "clike", "css", "livescript", "lua", "markdown", "mirc", "sql", "mathematica", "modelica", "mumps", "sql", "mbox", "sql", "nginx", "nsis", "ntriples", "clike", "mllike", "octave", "oz", "pascal", "pegjs", "perl", "php", "pig", "null", "sql", "powershell", "properties", "protobuf", "python", "puppet", "q", "r", "rst", "rpm", "rpm", "ruby", "rust", "sas", "sass", "clike", "scheme", "css", "shell", "sieve", "slim", "smalltalk", "smarty", "solr", "mllike", "soy", "sparql", "spreadsheet", "sql", "sql", "clike", "stylus", "swift", "stex", "stex", "verilog", "tcl", "textile", "tiddlywiki", "tiki", "toml", "tornado", "troff", "ttcn", "ttcn-cfg", "turtle", "javascript", "jsx", "twig", "webidl", "vb", "vbscript", "velocity", "verilog", "vhdl", "vue", "xml", "xquery", "yacas", "yaml", "z80", "mscgen", "mscgen", "mscgen"]
+            allModes: ["apl", "asciiarmor", "asn.1", "asterisk", "brainfuck", "clike", "clike", "cobol", "clike", "clojure", "clojure", "css", "cmake", "coffeescript", "commonlisp", "cypher", "python", "crystal", "css", "sql", "d", "dart", "diff", "django", "dockerfile", "dtd", "dylan", "ebnf", "ecl", "clojure", "eiffel", "elm", "htmlembedded", "htmlembedded", "erlang", "sql", "factor", "fcl", "forth", "fortran", "mllike", "gas", "gherkin", "gfm", "go", "groovy", "haml", "haskell", "haskell-literate", "haxe", "haxe", "htmlembedded", "htmlmixed", "http", "idl", "pug", "clike", "htmlembedded", "javascript", "jsx", "jinja2", "julia", "clike", "css", "livescript", "lua", "markdown", "mirc", "sql", "mathematica", "modelica", "mumps", "sql", "mbox", "sql", "nginx", "nsis", "ntriples", "clike", "mllike", "octave", "oz", "pascal", "pegjs", "perl", "php", "pig", "null", "sql", "powershell", "properties", "protobuf", "python", "puppet", "q", "r", "rst", "rpm", "rpm", "ruby", "rust", "sas", "sass", "clike", "scheme", "css", "shell", "sieve", "slim", "smalltalk", "smarty", "solr", "mllike", "soy", "sparql", "spreadsheet", "sql", "sql", "clike", "stylus", "swift", "stex", "stex", "verilog", "tcl", "textile", "tiddlywiki", "tiki", "toml", "tornado", "troff", "ttcn", "ttcn-cfg", "turtle", "javascript", "jsx", "twig", "webidl", "vb", "vbscript", "velocity", "verilog", "vhdl", "vue", "xml", "xquery", "yacas", "yaml", "z80", "mscgen", "mscgen", "mscgen"]
         };
     }
 
@@ -54,6 +54,7 @@ class CodeMirrorWrapper extends React.Component {
                             mode: this.state.mode,
                             theme: this.state.theme,
                             lineNumbers: true,
+                            readOnly: true
                         }}
                         onChange={(editor, data, value) => {
                         }}
